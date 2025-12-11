289b45a1dd216e52684b0742db8c05abfe1204a2
29kinc <29kinc@gmail.com>
2025-12-11 12:11:40 +0900

    대규모경매 다른브랜치로이동

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MainAuction from "./pages/MainAuction";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/main-auction" element={<MainAuction />}></Route>
-      </Routes>
+      <Routes></Routes>
     </Router>
   );
 }
